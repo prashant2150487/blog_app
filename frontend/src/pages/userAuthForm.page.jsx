5f01b8fdf3bad8f778a53f5ac796465d5eea4bb3
Prashnt Sachan <swepra04@gmail.com>
2024-04-23 11:52:18 +0530

    backend auth done

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,36 +1,96 @@
-import React from "react";
+import React, { useRef } from "react";
 import InputBox from "../components/input.component";
-import googleIcon from "../imgs/google.png"
+import googleIcon from "../imgs/google.png";
 import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
+import toast, { Toaster } from "react-hot-toast";
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
 const UserAuthForm = ({ type }) => {
+  const authForm = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let form = new FormData(authForm.current);
+    let formData = {};
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+    console.log(formData);
+    let { fullname, email, password } = formData;
+    if (fullname) {
+      if (fullname.length < 3) {
+        toast.error("Fullname atleast must be 3 letter long");
+      }
+    }
+
+    if (!email.length) {
+      toast.error("Enter email please");
+    }
+    if (!emailRegex.test(email)) {
+      toast.error("Email is invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      toast.error(
+        "Password should be atleast 6 character long and should contain atleast one uppercase, one lowercase and one digit"
+      );
+    }
+  };
+
   return (
     <AnimationWrapper keyValue={type}>
       <section className=" flex items-center justify-center h-cover">
-        <form className="w-[80%] max-w-[400px]">
+        <Toaster />
+        <form ref={authForm} className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type == "sign-in" ? "wellcome back" : "Join us today"}
           </h1>
-          {
-            type == "sign-up"
-              ?
-              <>
-                <InputBox
-                  name="Full Name"
-                  type="text"
-                  placeholder="Full Name"
-                  icon="fi-rr-user"
-                />
-                <InputBox name="email" type="email" placeholder="Email" icon='fi-rr-envelope' />
-                <InputBox name="password" type="password" placeholder="Password" icon='fi-rr-key' />
-              </>
-              :
-              <>
-                <InputBox name="email" type="email" placeholder="Email" icon='fi-rr-envelope' />
-                <InputBox name="password" type="password" placeholder="Password" icon='fi-rr-key' />
-              </>
-          }
-          <button className="btn-dark center mt-14">{type.replace("-", " ")}</button>
+          {type == "sign-up" ? (
+            <>
+              <InputBox
+                name="fullname"
+                type="text"
+                placeholder="Full Name"
+                icon="fi-rr-user"
+              />
+              <InputBox
+                name="email"
+                type="email"
+                placeholder="Email"
+                icon="fi-rr-envelope"
+              />
+              <InputBox
+                name="password"
+                type="password"
+                placeholder="Password"
+                icon="fi-rr-key"
+              />
+            </>
+          ) : (
+            <>
+              <InputBox
+                name="email"
+                type="email"
+                placeholder="Email"
+                icon="fi-rr-envelope"
+              />
+              <InputBox
+                name="password"
+                type="password"
+                placeholder="Password"
+                icon="fi-rr-key"
+              />
+            </>
+          )}
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {type.replace("-", " ")}
+          </button>
           <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
             <hr className="w-1/2 border-black" />
             <p>or</p>
@@ -40,22 +100,24 @@ const UserAuthForm = ({ type }) => {
             <img src={googleIcon} className="w-5" />
             continue with google
           </button>
-          {
-            type == 'sign-in' ?
-              <p className="mt-6 text-dark-grey text-xl text-center">
-                Don't have account ?
-                <Link to='/signup' className="underline text-black text-xl ml-1">Join us today</Link>
-              </p>
-              :
-              <p className="mt-6 text-dark-grey text-xl text-center">
-                Already a member ?
-                <Link to="/signin" className="underline text-black text-xl ml-1">Sign in here</Link>
-              </p>
-          }
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have account ?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member ?
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here
+              </Link>
+            </p>
+          )}
         </form>
       </section>
     </AnimationWrapper>
-
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -1,32 +1,106 @@
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
+import bcrypt from "bcrypt";
+import User from "./Schema/User.js";
+import { nanoid } from "nanoid"
+import jwt from "jsonwebtoken"
+
+
 
 const server = express();
 let PORT = 3000;
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
-server.use(express.json())
-mongoose.connect(process.env.DB_LOCATION, { autoIndex: true })
+server.use(express.json());
+mongoose.connect(process.env.DB_LOCATION, { autoIndex: true });
 
 
-server.post('/signup', (req, res) => {
+
+const formatDatattoSend = (user) => {
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+    return {
+        access_token,
+        prifile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
+
+    }
+}
+const generateUsername = async (email) => {
+    let username = email.split('@')[0];
+    let isUsernameNotUnique = await User.exists({ "personal_info.username": username }).then((result) => result)
+    if (isUsernameNotUnique) {
+        username = username + nanoid(5)
+    }
+    return username;
+
+}
+
+server.post("/signup", (req, res) => {
     const { fullname, email, password } = req.body;
     if (fullname.length < 3) {
-        res.status(403).json({ "error": "Fullname atleast must be 3 letter long" })
+        res.status(403).json({ error: "Fullname atleast must be 3 letter long" });
     }
     if (!email.length) {
-        return res.status(403).json({ "error": "Enter email please" })
+        return res.status(403).json({ error: "Enter email please" });
     }
     if (!emailRegex.test(email)) {
-        return res.status(403).json({ "error": "Email is invalid" })
+        return res.status(403).json({ error: "Email is invalid" });
     }
     if (!passwordRegex.test(password)) {
-        return res.status(403).json({ "error": "Password should be atleast 6 character long and should contain atleast one uppercase, one lowercase and one digit" })
+        return res
+            .status(403)
+            .json({
+                error:
+                    "Password should be atleast 6 character long and should contain atleast one uppercase, one lowercase and one digit",
+            });
     }
-    return res.status(200).json({ "status": "Okey" })
+    bcrypt.hash(password, 10, async (err, hashed_password) => {
+        let username = await generateUsername(email);
+        let user = new User({
+            personal_info: {
+                fullname,
+                email,
+                password: hashed_password,
+                username
+            },
+        });
+        user.save().then((u) => {
+            return res.status(200).json(formatDatattoSend(u));
+        }).catch((err) => {
+            if (err.code == 1100) {
+                return res.status(403).json({ "error": "Email already exists" })
+            }
+            return res.status(500).send({ error: err.message });
+        });
+    });
+});
+server.post("/signin", async (req, res) => {
+    let { email, password } = req.body;
+    await User.findOne({ "personal_info.email": email })
+        .then((user) => {
+            if (!user) {
+                return res.status(403).json({ "error": "Email not found" })
+            }
+            bcrypt.compare(password, user.personal_info.password, (err, result) => {
+                if (err) {
+                    return res.status(403).json({ "error": "Error occured while login please try again" })
+                }
+                if (!result) {
+                    return res.status(403).json({ "error": "Incorrect password" })
+                } else {
+                    return res.status(200).json(formatDatattoSend(user))
+                }
+            })
+        }).catch(err => {
+            return res.status(500).json({ "error": err.message });
+        })
+
+
+
 })
 
 server.listen(PORT, () => {
-    console.log(`server is listining on port -> ${PORT}`)
-})
+    console.log(`server is listining on port -> ${PORT}`);
+});
