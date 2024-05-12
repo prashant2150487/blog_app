841caa51dc6f90e640ad057f1bd4aef1debfdb60
Prashnt Sachan <swepra04@gmail.com>
2024-05-12 22:13:42 +0530

    editor js

--- frontend/src/components/tools.component.jsx
+++ frontend/src/components/tools.component.jsx
@@ -5,7 +5,34 @@ import Quote from "@editorjs/quote"
 import Marker from "@editorjs/marker"
 import InlineCode from "@editorjs/inline-code"
 import Header from '@editorjs/header';
+import { uploadImage } from "../common/aws"
 
+
+const uploadImageByFile = (e) => {
+    return uploadImage(e).then(url => {
+        if (url) {
+            return {
+                success: 1,
+                file: { url }
+            }
+        }
+    })
+}
+const uploadImageByURL = (e) => {
+    let link = new Promise((resolve, reject) => {
+        try {
+            resolve(e)
+        } catch (err) {
+            reject(err)
+        }
+    })
+    return link.then(url => {
+        return {
+            success: 1,
+            file: { url }
+        }
+    })
+}
 
 export const tools = {
     embed: Embed,
@@ -13,7 +40,15 @@ export const tools = {
         class: List,
         inlineToolBar: true
     },
-    image: Image,
+    image: {
+        class: Image,
+        config: {
+            uploader: {
+                uploadByUrl: uploadImageByURL,
+                uploadByFile: uploadImageByFile
+            }
+        }
+    },
     header: {
         class: Header,
         config: {
